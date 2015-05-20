8308b69a5e74d2ee492b476c31f1784d7079bdc5
Zerbinati Francesco <me@zerbinatifrancesco.it>
2015-05-20 19:27:22 +0200

    un sacco di cambiamenti come: pagine ora dinamiche in jquery, pagina contatti "completa", api google maps

---
+++ js/headerManager.js
@@ -0,0 +1,18 @@
+    $('#menu_nav li').on('click', function(){
+        // remove the css to the previous selected, add to the new one
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+
+        // get the href attribute of the link
+        var href = $(this).children().attr('href');
+
+        // get the actual page name
+        var page = href.substr(1);
+
+        // load the page dinamycally inside the template
+        $( ".main" ).load(page+'.html', function() {
+            // specific js load for pages
+            //for istance, if(page == 'contact')
+        });
+
+    });
